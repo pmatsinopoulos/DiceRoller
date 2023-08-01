31e604afc2d2331167b2bb112ba282f0ef371af5
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-08-01 18:05:22 +0300

    Button to roll the dice works.

--- app/src/main/java/com/mixlr/panos/diceroller/MainActivity.kt
+++ app/src/main/java/com/mixlr/panos/diceroller/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.mixlr.panos.diceroller
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -10,10 +11,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -21,6 +24,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mixlr.panos.diceroller.ui.theme.DiceRollerTheme
+import java.lang.IllegalArgumentException
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,16 +39,29 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    var result by remember { mutableStateOf(1) }
+    val imageResource = when (result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        6 -> R.drawable.dice_6
+        else -> throw IllegalArgumentException("result $result should be between 1 and 6 including")
+    }
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
-            contentDescription = "1",
-            painter = painterResource(id = R.drawable.dice_1)
+            contentDescription = result.toString(),
+            painter = painterResource(id = imageResource)
         )
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { /*TODO*/ }) {
+        Button(onClick = {
+            result = (1..6).random()
+            Log.d("DiceRollerApp", result.toString())
+        }) {
             Text(
                 text = stringResource(id = R.string.roll)
             )
